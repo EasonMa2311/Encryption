03717fd2013595fe283e0a0d2f36163db7651df8
EasonMa2311 <19Ema@haz5d.com>
2024-10-25 13:21:39 +0000

    add funciton for update/insert user data

--- User_database.py
+++ User_database.py
@@ -15,13 +15,14 @@ class DataRecord:
         """)
         self.DataBase.commit()
         self.cc.execute("""
-            CREATE TABLE IF NOT EXISTS UserSetting ( 
+            CREATE TABLE IF NOT EXISTS UserSettingData ( 
                 Userid INTEGER PRIMARY KEY AUTOINCREMENT,
-                theme TEXT,
-                font TEXT,
+                Theme TEXT,
+                font INTEGER
             )
         """)
         self.DataBase.commit()
+        
 
     def check_user(self,user):
         self.cc.execute('''SELECT UserName FROM UserData ''')
@@ -49,3 +50,44 @@ class DataRecord:
             return True
         else:
             return False
+        
+    def insert_user_setting(self,Theme,font,username):
+        execute_text = f'SELECT Userid FROM UserData WHERE UserName = "{username}";'
+        self.cc.execute(execute_text)
+        user_id = [row[0] for row in self.cc.fetchall()]
+        execute_text = f'SELECT Userid FROM UserSEttingData WHERE Userid = "{user_id}";'
+        self.cc.execute(execute_text)
+        check_user_id = [row[0] for row in self.cc.fetchall()]
+
+        
+        user_id = user_id[0]
+        if check_user_id != []:
+            
+            execute_text = f'UPDATE UserSEttingData SET Theme={Theme},font={font} WHERE Userid = {user_id}'
+            self.cc.execute(execute_text)
+            self.DataBase.commit()
+        else:
+            self.cc.execute('INSERT INTO UserSettingData (Userid, Theme ,font) VALUES (? , ?, ?);', (user_id,Theme, font))
+            self.DataBase.commit()
+
+    def update_username(self,username,new_username):
+        execte_text = f'UPDATE UserData SET UserName="{new_username}" WHERE UserName = "{username}"'
+        self.cc.execute(execte_text)
+        self.DataBase.commit()
+
+
+    def update_password(self,username,password):
+        execte_text = f'UPDATE UserData SET Password="{password}" WHERE UserName = "{username}"'
+        self.cc.execute(execte_text)
+        self.DataBase.commit()
+
+    def select_user_data(self,username):
+        execute_text = f'SELECT * FROM UserData WHERE UserName = "{username}";'
+        self.cc.execute(execute_text)
+        user_data = self.cc.fetchall()[0]
+        user_id = user_data[0]
+        userName = user_data[1]
+        userPassword = user_data[2]
+        
+        return user_id,userName,userPassword
+        
